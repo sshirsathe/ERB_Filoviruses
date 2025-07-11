d7e7b77fc96e25c7639234e487a243bf37dc2148
Shruti Shirsathe <63205434+sshirsathe@users.noreply.github.com>
2025-07-10 20:48:40 -0400

    Update transcript_counts.R

--- Preprocessing_and_Quantification/transcript_counts.R
+++ Preprocessing_and_Quantification/transcript_counts.R
@@ -2,30 +2,27 @@
 #This code uses combinations of ideas, techniques and code snippets
 #from: Juan Angel Patiño-Galindo
 
-library(dplyr) # data wrangling
-library(ggplot2) # plotting
-library(DESeq2) # rna-seq
-library(edgeR) # rna-seq
-library(tximport) # importing kalisto transcript counts to geneLevels
-library(readr) # Fast readr of files.
-library(rhdf5) # read/convert kalisto output files.
+library(dplyr) 
+library(DESeq2)
+library(edgeR)
+library(tximport) 
+library(readr)
+library(rhdf5) 
 
 dir <- system.file("extdata", package = "tximportData")
-#dir
 
 samples <- read.csv("/sc/arion/projects/BAT-NATIVE/SRR_Data/Lee_2015_Metadata.csv", header=TRUE,stringsAsFactors=F,row.names=1)
-samples
 
 setwd("/sc/arion/projects/BAT-NATIVE/SRR_Data")
 files <- paste("outputs",list.files(path = "outputs",pattern = "abundance.tsv", recursive = TRUE),sep = "/")
 names(files) <- samples$Run
-files
 
 tx2gene <- read.csv("/sc/arion/projects/BAT-NATIVE/SRR_Data/tx2gene.csv")
 
-
+# import kalisto transcript counts to geneLevels
 txi.kallisto.tsv <- tximport(files, type = "kallisto", tx2gene = tx2gene, ignoreAfterBar = TRUE)
 
+#convert transcript to gene in labels from tx2gene.csv file
 idx <- rownames(txi.kallisto.tsv$counts) %in% tx2gene[,1]
 txi.sub <- txi.kallisto.tsv
 for (mat in c("abundance","counts","length")) {
@@ -33,17 +30,11 @@ for (mat in c("abundance","counts","length")) {
 }
 txi.sub
 
-
 df <- data.frame(
   gene_counts = txi.kallisto.tsv$counts
 )
-#df
 
 df2 <- data.frame(sub("\\_.*", "", basename(files)))
-
-df2
-
-#colnames(df2)
 
 colnames(df) <- df2$sub...._.........basename.files..
 
